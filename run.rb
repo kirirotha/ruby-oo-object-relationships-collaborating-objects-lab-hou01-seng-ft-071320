b0911deba0d8888f9c4c1611756696f05516c39d
Kiri Um <kirirotha@gmail.com>
2020-07-19 17:33:03 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -5,13 +5,13 @@ require_relative 'lib/song.rb'
 
 
 
-binding.pry
+#binding.pry
 
 s1 = Song.new_by_filename("Where its at - Beck")
 s2 = Song.new_by_filename("Funeral - Band of Horses")
 s3 = Song.new_by_filename("1234 - Feist")
 s2 = Song.new_by_filename("Spotieotiedopalicious - Outkast")
-
+binding.pry
 beck = Artist.new("Beck")
 feist = Artist.new("Feist")
 
