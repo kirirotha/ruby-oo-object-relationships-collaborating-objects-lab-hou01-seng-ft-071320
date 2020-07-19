ddf202ccaa7292d87ab34c38f75707a63c6a78ca
Kiri Um <kirirotha@gmail.com>
2020-07-19 17:55:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,6 +40,11 @@ class Artist
 
   def print_songs
     binding.pry
+    song_list
+    songs.each do |a_song|
+      song_list = "#{song_list}#{a_song.name}\n"
+    end
+    song_list
   end
 
 end
